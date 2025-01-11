6be16efba195757d9da1835d5b046660e0f7015a
MagalDevs <danielgalfla12@gmail.com>
2025-01-10 21:47:15 -0300

    estudos 10-01-25

--- src/main/java/br/com/alura/Screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/Screenmatch/ScreenmatchApplication.java
@@ -1,9 +1,5 @@
 package br.com.alura.Screenmatch;
 
-import br.com.alura.Screenmatch.Main.Main;
-import br.com.alura.Screenmatch.repository.SerieRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
